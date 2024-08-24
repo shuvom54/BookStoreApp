24900db0a56a4df0b42b53e6991e1cb3bb2c92f0
Shuvom <shuvom2023@gmail.com>
2024-08-24 21:31:40 +0530

    change

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -2,16 +2,22 @@ import  express from "express";
 import mongoose from "mongoose";
 import  dotenv from "dotenv";
 import cors from "cors";
-
+import path from "path";
 import bookRoute from "./route/book.route.js";
 import userRoute from "./route/user.route.js";
 
 
 
 const app = express();
+// app.use(cors(
+//   {
+//     origin:["https://deploy"]
+//   }
+// ));
 
 app.use(cors());
 app.use(express.json());
+mongoose.connect('mongodb+srv://shuvom2023:shuvom@2002@cluster0.76pix.mongodb.net/');
 
 dotenv.config();
 
@@ -35,7 +41,14 @@ try {
 app.use("/book",bookRoute);
 app.use("/user",userRoute);
 
-
+//deployment
+// if(process.env.NODE_ENV=="production"){
+//      const dirPath=path.resolve();
+//      app.use(express.static("FrontEnd/dist"));
+//      app.get("*",(req,res)=>{
+//       res.sendFile(path.resolve(dirPath,"FrontEnd","dist","index.html"));
+//      })
+// }
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`)
