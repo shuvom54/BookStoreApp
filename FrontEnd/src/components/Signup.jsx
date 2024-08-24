1820e0da9aa759533fcd75e1825ec22261de8b8a
Shuvom_Dutta <140581655+shuvom54@users.noreply.github.com>
2024-08-24 23:02:21 +0530

    Update Signup.jsx

--- FrontEnd/src/components/Signup.jsx
+++ FrontEnd/src/components/Signup.jsx
@@ -24,7 +24,7 @@ function Signup() {
       password:data.password
     };
     await axios
-    .post("http://localhost:4001/user/signup",userInfo)
+    .post("book-store-app-api-lovat.vercel.app/user/signup",userInfo)
     .then((res)=>{
       console.log(res.data);
       if(res.data){
